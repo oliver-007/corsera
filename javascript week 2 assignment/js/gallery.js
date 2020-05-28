1e1374fa96dbdd0cfb28426a4734b48ca24b7fdc
Faisal Ahmed <oliverahmed000@gmail.com>
2020-05-28 13:54:41 +0600

    final js folder added

--- javascript week 2 assignment/js/gallery.js
+++ javascript week 2 assignment/js/gallery.js
@@ -1,20 +1,3 @@
-// function openMe() {
-//     x = document.getElementById("demo");
-//     // x.style.display = "block";
-//     x.className = "open";
-// }
-
-// function closeMe() {
-//     x = document.getElementById("demo");
-//     // x.style.display = "none";
-//     x.className = "close";
-// }
-// function showDate() {
-//     document.getElementById("day").innerHTML= Date();
-// }
-
-// assignmetn demo code
-
 /*Name this external file gallery.js*/
 
 function upDate(previewPic){
@@ -25,11 +8,13 @@ function upDate(previewPic){
        2) Change the text  of the div with the id = "image" 
        to the alt text of the preview image 
        */
-     document.getElementById("image").style.backgroundImage = "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon.jpg')";
-      image.innerHTML = "Styling with a Bandana";
-     
-       }
-   
+    
+          document.getElementById('image').innerHTML = previewPic.alt;
+          document.getElementById("image").style.backgroundImage = "url('"+previewPic.src+"')";
+    }
+
+    
+
        function unDo(){
         /* In this function you should 
        1) Update the url for the background image of the div with the id = "image" 
@@ -38,7 +23,7 @@ function upDate(previewPic){
        2) Change the text  of the div with the id = "image" 
        back to the original text.  You can use the html code to see what that original text was
        */
-      x = document.getElementById("image");
-      x.style.backgroundImage = "url('')";
-      x.innerHTML = "Hover over an image below to display here.";
+      
+         document.getElementById('image').innerHTML = "Hover over an image below to display here."
+	       document.getElementById("image").style.background = "#8e68ff";
        }
